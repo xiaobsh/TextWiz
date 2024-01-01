6122173ad6d3ed367b788ba63239712e663be21d
xiaobsh <88483240+xiaobsh@users.noreply.github.com>
2024-01-01 14:46:14 +0800

    Refactor the project and rename it "TextWiz". Redesign the test program (can interact).

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,20 +1,83 @@
 #include <iostream>
-#include ".\..\Main\IdealFile.cpp"
+#include ".\..\TextWiz\TextWiz.cpp"
+#include <conio.h>
 
 using namespace std;
 
+TextWiz* p_tw;
+
+void exitfunc()
+{
+	// (*Arg).close();
+	p_tw -> close();
+}
+
+
 int main()
 {
-	IdealFile F;
+	TextWiz F;
+	p_tw = &F;
 	F.open("hosts");
-	cout << F.getbyline(2) << endl;
-	cout << F.getcontent() << "###@EOF1" << endl;
-	F.addatline(2, "Testing!!!");
-	cout << F.getbyline(2) << endl;
-	F.delline(3);
-	cout << "--------------------" << endl << F.getcontent() << "###@EOF2" << endl;
-	F.save();
-	F.close();
-	system("pause");
+	atexit(exitfunc);
+
+	int choice;
+	cout << "Test Program for TextWiz by xiaobsh" << endl;
+	cout << "0 Exit\t\t1 Get content\t2 Get by line\n3 Add a line\t4 Delete a line\t5 Save to file" << endl << "Please enter your choice: ";
+	// cin >> choice;
+	choice = _getch(); cout << choice - '0' << endl;
+
+	while (choice != '0')
+	{
+		switch (choice - '0')
+		{
+		case 1:
+		{
+			cout << "================================Beginning================================" << endl;
+			cout << F.get_current_content() << endl;
+			cout << "==================================Ending==================================" << endl;
+			break;
+		}
+		case 2:
+		{
+			int line;
+			string s;
+			cout << "Please enter the line number: ";
+			cin >> line;
+			cout << "================================Beginning================================" << endl;
+			cout << F.getbyline(line) << endl;
+			cout << "==================================Ending==================================" << endl;
+			break;
+		}
+		case 3:
+		{
+			int line;
+			string s;
+			cout << "Please enter the line number: ";
+			cin >> line;
+			cout << "To add: ";
+			cin >> s;
+			F.addatline(line, s);
+			break;
+		}
+		case 4:
+		{
+			int line;
+			string s;
+			cout << "Please enter the line number: ";
+			cin >> line;
+			F.delline(line);
+			break;
+		}
+		case 5: F.save(); break;
+		default: cout << "Wrong order!" << endl; break;
+		}
+		cout << endl << "Test Program for TextWiz by xiaobsh" << endl;
+		cout << "0 Exit\t\t1 Get content\t2 Get by line\n3 Add a line\t4 Delete a line\t5 Save to file" << endl << "Please enter your choice: ";
+		// cin >> choice;
+		choice = _getch(); cout << choice - '0' << endl;
+	}
+
+	// F.exit();
+	// system("pause");
 	return 0;
 }
---
+++ TextWiz/TextWiz.cpp
@@ -0,0 +1,105 @@
+#include <string>
+#include <fstream>
+#include <vector>
+#include <ctime>
+
+// using namespace std;
+
+class TextWiz
+{
+public:
+	// About Files
+	void open(std::string filename); // r + w 读写
+	void close(); // 不使用析构函数是为了便于再次打开其他的文件。close 不会自动保存，exit 实现了保存退出。
+	std::string get_current_content();
+	void save();
+	void exit();
+
+	// Lines
+	std::string getbyline(int line);
+	void delline(int line);
+	void addatline(int line, std::string text);
+
+private:
+	std::fstream cufile;
+	std::string fname; // 方便重载
+
+	// 使用 vector 是为了处理不定长、不定行的文本；
+	// 这里的 cufile 仅读取一次，随后闲置等待写入；
+	// 
+	// 不定长的 vector 放在 class 的最后。
+	std::vector<std::string> content;
+};
+
+
+void TextWiz::open(std::string filename)
+{
+	cufile.open(filename, std::ios::in);
+	fname = filename;
+	content.clear();
+
+	// 预读取
+	std::string s;
+	while (getline(cufile, s))
+	{
+		content.push_back(s);
+	}
+	cufile.close();
+}
+
+void TextWiz::close()
+{
+	cufile.close();
+}
+
+std::string TextWiz::get_current_content()
+{
+	std::string s;
+	for (int i = 0; i < content.size(); ++i)
+	{
+		s += content[i] + "\n";
+	}
+	return s;
+}
+
+
+std::string TextWiz::getbyline(int line)
+{
+	return content[line - 1];
+}
+
+void TextWiz::delline(int line)
+{
+	content.erase(content.begin() + line - 1);
+}
+
+void TextWiz::addatline(int line, std::string text)
+{
+	content.insert(content.begin() + line - 1, text);
+}
+
+
+void TextWiz::save()
+{
+	cufile.open(fname, std::ios::out | std::ios::in);
+	cufile << get_current_content();
+	cufile.close();
+}
+
+void TextWiz :: exit()
+{
+	save();
+	close();
+}
+
+
+char* getTime()
+{
+	time_t p_time;
+	tm p_tm;
+	time(&p_time);
+	char tmp[256];
+	localtime_s(&p_tm, &p_time);
+	strftime(tmp, sizeof(tmp), "%Y-%m-%d %H:%M:%S", &p_tm);
+	return tmp;
+}
